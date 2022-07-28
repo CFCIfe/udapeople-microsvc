aa4122c0fe19bf24bd50313b46848cb70e8f8d07
Peter Abolude <aboludepeter@gmail.com>
2022-07-28 17:30:45 +0100

    run_k8s.sh error

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# This tags and uploads an image to Kubernetes
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="cfcife/cfcife-microsvc:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl create deploy cfcife-microsvc --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/cfcife-microsvc --address 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="cfcife/cfcife-microsvc:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker tag cfcife-microsvc $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
+docker login
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
